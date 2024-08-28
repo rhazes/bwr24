81be04138b467787eac0491f743a4934f23966f1
KylerZan <kzanuck25@windwardschool.org>
2024-08-28 13:13:39 -0700

    ok

--- src/sketch.js
+++ src/sketch.js
@@ -2,12 +2,12 @@ function setup() {
 	new Canvas(400, 400);
 
 	sprite = new Sprite();
-	sprite.diameter = 100;
+	sprite.diameter = 200;
   noStroke();
 }
 
 function draw() {
-  background(220);  
+  background(250);  
   sprite.x =  0.5 * frameCount % width;
   if(frameCount % 30 == 0) {
     sprite.y = random(height);

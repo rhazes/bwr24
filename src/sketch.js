a8ebc1d4bea1ef4b799ce7bd80b727931c623dd4
justinmerr1tt <145143680+justinmerr1tt@users.noreply.github.com>
2024-08-28 13:17:38 -0700

    wasd movment, jump border at canvas bottom

--- src/sketch.js
+++ src/sketch.js
@@ -2,17 +2,35 @@ function setup() {
 	new Canvas(400, 400);
 
 	sprite = new Sprite();
-	sprite.diameter = 100;
+	sprite.diameter = 30;
   noStroke();
+  world.gravity.y = 1;
 }
 
 function draw() {
-  background(220);  
-  sprite.x =  0.5 * frameCount % width;
-  if(frameCount % 30 == 0) {
-    sprite.y = random(height);
+  background(255);  
+  //sprite.x =  0.5 * frameCount % width;
+  if(key == 'w') {
+    sprite.y = sprite.y-5;
   }
-  sprite.color = color(random(255),0,0);
+  
+  if(key == 'a') {
+    sprite.x = sprite.x-4;
+  }
+
+  if(key == 'd') {
+    sprite.x = sprite.x+4;
+  }
+
+  if(key == 's') {
+    sprite.y = sprite.y+5;
+  }
+
+  if(sprite.y > 380) {
+    sprite.y = sprite.y-20;
+  }
+
+  sprite.color = color(200, 0 , 0);
 }
 
 let sprite;

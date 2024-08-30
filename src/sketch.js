09bec6bef03c59868bf87b30ab99d4df54a9e2c6
Jhung-25 <jhung25@windwardschool.org>
2024-08-29 21:42:32 -0700

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -10,8 +10,8 @@ function setup() {
   ground = createSprite(0,400,800,10,'static');
 
   p1 = createSprite(300,300,70,10, 'static');
-  p1 = createSprite(100,330,70,10, 'static');
-  p1 = createSprite(200,200,70,10, 'static');
+  p2 = createSprite(100,330,70,10, 'static');
+  p3 = createSprite(200,200,70,10, 'static');
 
 
 
@@ -21,6 +21,7 @@ function setup() {
 
 function draw() {
   background(255);  
+  camera.position.x = sprite.position.x;
 
   if (sprite.position.y > height-20) {
     sprite.position.y = height-20;
@@ -28,16 +29,30 @@ function draw() {
   }
 
  
-  if(keyIsDown(87))
-    sprite.y = sprite.y-5;
-  if(keyIsDown(65))
-    sprite.x = sprite.x-4;
-  if(keyIsDown(68))
-    sprite.x = sprite.x+5;
-  if(keyIsDown(83))
-    sprite.x = sprite.x+4;
-  if(keyIsDown(32))
+  // Left and right movement
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // 65 is the keyCode for 'A'
+    sprite.velocity.x = -5;
+  } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // 68 is the keyCode for 'D'
+    sprite.velocity.x = 5;
+  } else {
     sprite.velocity.x = 0;
+  }
 
-  sprite.color = color(200, 0 , 0);
+  // Jumping
+  if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && (sprite.collide(allSprites))) { // 87 is the keyCode for 'W'
+    sprite.velocity.y = -15;
+  }
+
+  // Apply gravity
+  sprite.velocity.y += 0.8;
+
+  // Limit falling speed
+  if (sprite.velocity.y > 15) {
+    sprite.velocity.y = 15;
+  }
+
+  // Collision with ground
+  sprite.collide(ground);
+
+  sprite.color = color(200, 0, 0);
 }
